5eb1ae01b0b9b7bf616248c6f70f06b12e828f31
Jedd Orsmond <70684449+jeddors@users.noreply.github.com>
2022-03-30 15:19:44 +0200

    refactoring of project

--- 2 - Preprocess Data/EntryPoint.m
+++ 2 - Preprocess Data/EntryPoint.m
@@ -1,16 +1,23 @@
 clear all;
 clc;
 
+addpath 'C:\Users\JOrsmond\MATLAB\Projects\masters'\plotting\
+addpath 'C:\Users\JOrsmond\MATLAB\Projects\masters'\dataCommit\
+
 %1 - get historical generated data set
 load ('..\dataCommit\five_historicalbatches.mat'); % --> history
 
 %2 - align trajectories
-%load ('..\dataCommit\five_historicalbatches_aligned.mat'); %--> historySP
-load ('..\dataCommit\five_historicalbatches_aligned_trimmed.mat'); %--> history_trim
+s_threshold = 0.3;
+timestep = 0.01;
+historySP = AlignBySingularPoint(history, s_threshold, timestep)
+%load (genpath('..\dataCommit\five_historicalbatches_aligned.mat')); %--> historySP
+load ('dataCommit\five_historicalbatches_aligned_trimmed.mat'); %--> history_trim
+%PlotAllBatches(historySP);
 
 %3 - Unfold data variable-wise
 %unfolded = UnfoldBatches(history_trim); --> unfolded
-load ('..\dataCommit\five_historicalbatches_aligned_trimmed_unfolded.mat');
+load ('dataCommit\five_historicalbatches_aligned_trimmed_unfolded.mat');
 
 %4 - Standardize unfolded data
 [unfolded_std, centers, scales] = StandardizeUnfolded(unfolded);
@@ -21,3 +28,7 @@ load ('..\dataCommit\five_historicalbatches_aligned_trimmed_unfolded.mat');
 inputVars = ["CL", "CO2", "S", "T", "V", "pH", "X", "F", "Pw"];
 outputVars = ["P"];
 [inputTable, outputTable] = SplitTables(unfolded_std, inputVars, outputVars);
+
+%6 - Run PCA
+
+
